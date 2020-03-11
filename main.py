f497379bdcbdf2ce021971d7ded28a4b1fa53cd8
d.zwart <davidzwa@gmail.com>
2020-03-11 15:33:21 +0100

    Fix final exceptions appearing

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import os, sys, pprint
-import mypy
-
 from mypy import api
 
-result = api.run(["analysis-test.py"])
-if result[0]:
-    print('Type checking report:')
-    print(result[0])  # stdout
-if result[1]:
-    print('Error report:')
-    print(result[1])  # stderr
-print('Exit status:', result[2])
+if __name__ == "__main__":
+    result = api.run(["analysis-test.py"])
+    if result[0]:
+        print('Type checking report:')
+        print(result[0])  # stdout
+    if result[1]:
+        print('Error report:')
+        print(result[1])  # stderr
+    print('Exit status:', result[2])
