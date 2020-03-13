f10f0a271099fe589d5e92c7881bb5ef9111860b
David Zwart <davidzwa@gmail.com>
2020-03-13 15:51:04 +0100

    Add some issue-specific flags to api call

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import os, sys
 from pprint import pprint
-
-sys.path.append('/app/mypy')
-
-import mypy
-
+sys.path.append('./mypy')
 from mypy import api
 
 if __name__ == "__main__":
-    result = api.run(["analysis-test.py"])
+    result = api.run(["analysis-test.py", "--no-site-packages", "--verbose"])
     if result[0]:
         print('Type checking report:')
         print(result[0])  # stdout
